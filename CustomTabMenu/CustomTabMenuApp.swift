b9c95130e563a6fd7cbbd7d44b9b01f0f5a31952
BGDV <andrey.al.bogdanov@gmail.com>
2022-10-22 21:26:54 +0300

    Initial Commit

---
+++ CustomTabMenu/CustomTabMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabMenuApp.swift
+//  CustomTabMenu
+//
+//  Created by BGDV on 22.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
